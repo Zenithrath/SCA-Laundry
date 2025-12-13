094b19dd3f32f8779b253ad1053ba59e0357d82f
Zenithrath <zenithrat@gmail.com>
2025-12-13 13:33:47 +0700

    js

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
     <title>{{ config('app.name', 'SCA Laundry') }}</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
         body { font-family: 'Poppins', sans-serif; transition: background-color 0.3s ease, color 0.3s ease; }
